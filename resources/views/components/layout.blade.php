ca913082377baaad1399588cb53782eb84b55915
Roberto Rocha <rrocha@correo.xoc.uam.mx>
2026-04-14 13:23:14 -0600

    Arreglo de sesiones y login

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -96,7 +96,7 @@
                     </div>
 
                     <button type="submit" id="btnAcceder" class="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 flex justify-center items-center disabled:opacity-50">
-                        <span class="btn-text">Acceder</span>
+                        <span class="btn-text">Entrar</span>
                         <svg hidden class="btn-spinner animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>
                     </button>
                 </form>
@@ -236,7 +236,14 @@
                {{ $slot }}
             </div> 
         
-            <form id="logoutForm" action="/logout" method="POST" class="absolute 
+            <form method="POST" action="/logout">
+                @csrf  
+                @method('DELETE')
+                    <button type="submit" id="btnAcceder" class="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 flex justify-center items-center disabled:opacity-50">
+                        <span class="btn-text">Salir</span>
+                        <svg hidden class="btn-spinner animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>
+                    </button>
+            </form>
         @endauth|
 
   </div> 
